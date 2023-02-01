e9230eb54c8468a13fa58b9f0bb780223fc50e96
Youthhing <boysoeng@naver.com>
2023-02-01 23:10:36 +0900

    3장까지 완료.

---
+++ src/main/java/com/jojoldu/book/springboot/Apllication.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class Apllication {
+    public static void main(String []args){
+        SpringApplication.run(Apllication.class,args);
+    }
+}
